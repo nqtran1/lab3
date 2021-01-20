bd466162a8a82b2a60e1a43ec776b7455736a4ab
nqtran1 <nqtran@ucsd.edu>
2021-01-20 15:57:57 -0800

    added stuff

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -12,7 +12,14 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
