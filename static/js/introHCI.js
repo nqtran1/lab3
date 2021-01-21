bc592649d2c1c11fda1cf3a963c1d583e84d2f5f
nqtran1 <nqtran@ucsd.edu>
2021-01-20 16:03:43 -0800

    fixed new var

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -24,6 +24,8 @@ function projectClick(e) { 
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
-    } else {
+    } 
+    else {
        description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
     }
+}
