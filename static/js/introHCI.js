9959be920717142a83b51aa025d61f80db1462b2
nqtran1 <nqtran@ucsd.edu>
2021-01-20 16:29:25 -0800

    fading

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -28,6 +28,6 @@ function projectClick(e) { 
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } 
     else {
-       $("description").fadeOut()
+       $(description).fadeOut()
     }
 }
